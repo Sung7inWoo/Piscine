dc6a96cfa2e0925a21d3f4e621ac65aa6f9c34e3
Sung7inWoo <bigjirut@gmail.com>
2023-01-28 21:39:22 +0700

    C05 ex00-07

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jthanikp <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 18:50:53 by jthanikp          #+#    #+#             */
+/*   Updated: 2023/01/27 23:09:48 by jthanikp         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	else if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d\n", ft_fibonacci(-12));
+	printf("%d\n", ft_fibonacci(0));
+	printf("%d\n", ft_fibonacci(1));
+	printf("%d\n", ft_fibonacci(5));
+	printf("%d\n", ft_fibonacci(10));
+	return (0);
+}*/
